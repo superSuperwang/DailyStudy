c1135ab2482db27bd1dbc6aed1ae3e1221702ba2
wangziyi <1299194997@qq.com>
2021-01-05 11:36:32 +0800

    二分查找变形

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -1,9 +1,8 @@
 Array.prototype.binarySearch = function (val) {
   let start = 0
-  console.log(this)
   let end = this.length - 1
   while (start <= end) {
-    let mid = Math.floor((end + start) / 2)
+    let mid = start + ((end - start) >> 1)
     if (this[mid] > val) {
       end = mid - 1
     } else if (this[mid] < val) {
@@ -14,6 +13,41 @@ Array.prototype.binarySearch = function (val) {
   }
   return -1
 }
-let arr = [1, 2, 3, 4, 5]
-console.log(arr.binarySearch(4))
+let arr = [1, 2, 3, 3, 3, 5]
 
+// 二分查找变形
+// 查找第一个符合的元素
+Array.prototype.binarySearchFirst = function (val) {
+  let start = 0
+  let end = this.length - 1
+  while (start <= end) {
+    let mid = start + ((end - start) >> 1)
+    if (this[mid] > val) {
+      end = mid - 1
+    } else if (this[mid] < val) {
+      start = mid + 1
+    } else {
+      if ((mid === 0) || this[mid - 1] !== val) return mid
+      end = mid - 1
+    }
+  }
+  return -1
+}
+
+console.log(arr.binarySearchFirst(3))
+
+// 查找第一个大于等于给定元素的下标
+Array.prototype.search = function (val) {
+  let low = 0
+  let high = this.length - 1
+  while (low <= high) {
+    let mid = low + ((high - low) >> 1)
+    if (this[mid] >= val) {
+      if ((mid === 0) || this[mid - 1] < val) return mid
+      high = mid - 1
+    } else {
+      low = mid + 1
+    }
+  }
+}
+console.log([3, 4, 6, 7, 10].search(5))
